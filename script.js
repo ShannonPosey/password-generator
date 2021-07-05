70313e9df742e3c3c6bd418c5a040762c642f89d
shannon_p <snp1284kasey@aol.com>
2021-07-05 01:57:49 -0400

    added conditional statements

--- script.js
+++ script.js
@@ -11,31 +11,93 @@ var specialChar = "<,>.?/:;'{[}]|\+=_-)(*&^%$#@!~`";
 
 
 var generatePassword = function () {
-  // ask for user input
-  var length = window.prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
-  length = parseInt(length);
-  if (!length){
-    window.alert("You need to enter a valid answer! Please try again.");
-    return(length);
+// create two new variable for this prompt
+var randomPassword = "";
+var randomChar = "";
+  // create prompt asking user to choice password length
+  var  randomLength = window.prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
+  randomLength = parseInt(randomLength);
+
+  // if password length is less than 8 characters alert user to choose again
+  if ( randomLength < 8) {
+    window.alert("Password length must be more than 8 characters. Please try again!");
+    return generatePassword();
   }
-  console.log(length);
-    window.confirm("Would you like the password to contain numbers?");
-    window.confirm("Would you like the password to contain lowercase letters?");
-    window.confirm("Would you like the password to contain uppercase letters?");
-    window.confirm("Would you like the password to contain special characters?");
-};
+  
+  // if password length is more than 128 characters alert user to choose again 
+
+  if ( randomLength > 128) {
+    window.alert("Password length must be less than 128 characters. Please try again!");
+    return generatePassword ();
+  }
+  console.log(randomLength);
+
+// create confirm numbers in the password "yes or no"
+// create a new variable for this prompt
+var numberChoice = window.confirm("Would you like the password to contain numbers?");
+if(numberChoice)
+  { randomChar += numericalChar;
+  }
+console.log(numberChoice);
+
+// create confirm lowercase letters in the password "yes or no"
+// create a new variable for this prompt
+var lowercaseChoice = window.confirm("Would you like the password to contain lowercase letters?");
+    // create a 
+    if(lowercaseChoice)
+    {randomChar += lowercaseChar;}
+
+    console.log(lowercaseChoice);
+
+// create confirm uppercase letters in the password "yes or no"
+// create a new variable for this prompt
+var uppercaseChoice = window.confirm("Would you like the password to contain uppercase letters?");
+    
+     if(uppercaseChoice){randomChar += uppercaseChar;}
+
+     console.log(uppercaseChoice);
+
+    // create confirm special characters in the password "yes or no"    
+    // create a new variable for the prompt
+    var specialCharChoice = window.confirm("Would you like the password to contain special characters?");
+    // create a 
+    if(specialCharChoice){randomChar += specialChar;}
+
+debugger;
+  for (var i = 0; i < randomLength; i++) {
+    randomPassword = randomChar (Math.floor(Math.random() * randomChar.length));
+  } 
+  
+  };
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(randomPassword) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  return("");
 
 }
+
+console.log(writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
